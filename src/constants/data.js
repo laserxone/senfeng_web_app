2878ffaed41ec5d6212ee95f3b4a2279a9b186fd
ubaid1840 <107393880+ubaid1840@users.noreply.github.com>
2025-03-20 07:03:02 +0500

    changed base url

--- src/constants/data.js
+++ src/constants/data.js
@@ -2798,5 +2798,5 @@ export const PakCities = [
   }
 ]
 
-// export const BASE_URL = "https://senfeng-web.vercel.app/api"
-export const BASE_URL = "http://localhost:3001/api"
+export const BASE_URL = "https://senfeng-web.vercel.app/api"
+// export const BASE_URL = "http://localhost:3001/api"
