98483901916ae20a59cf6c0badaa45f2992c6d16
ubaid1840 <107393880+ubaid1840@users.noreply.github.com>
2025-03-20 06:29:08 +0500

    checking

--- src/app/aftersales/page.jsx
+++ src/app/aftersales/page.jsx
@@ -1,5 +1,8 @@
-import { redirect } from "next/navigation";
+"use client";
+import { redirect, useRouter } from "next/navigation";
+import { useEffect } from "react";
 
-export default function Page (){
-    redirect("/aftersales/dashboard")
+export default function Page() {
+  const router = useRouter();
+  router.push("/aftersales/dashboard");
 }
