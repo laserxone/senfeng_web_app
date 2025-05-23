19bc45cec30195dd3f887ec95f3aa9e05c1a782a
ubaid1840 <ubaidrehman1840@gmail.com>
2025-05-23 17:03:51 +0500

    apk url updated

--- src/app/mobileapp/page.jsx
+++ src/app/mobileapp/page.jsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 export default function SenFengAppPage() {
   const href =
-    "https://github.com/laserxone/senfeng-app-apk/releases/download/v4.9.0/Releasev4.9.0.apk";
+    "https://github.com/laserxone/senfeng-app-apk/releases/download/v5.0.0/v5.0.0.apk";
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-900 to-gray-800 flex flex-col items-center justify-center text-white px-4">
