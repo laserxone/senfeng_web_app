f47bb414747bc8618beff3a65d3b1903cf40b900
ubaid1840 <ubaidrehman1840@gmail.com>
2025-06-26 22:41:35 +0500

    wrapper updated

--- src/components/maintenance-wrapper.jsx
+++ src/components/maintenance-wrapper.jsx
@@ -21,13 +21,17 @@ export default function MaintenanceWrapper({ children }) {
   const [mounted, setMounted] = useState(false);
   const [isMaintenance, setIsMaintenance] = useState(true);
 
+  useEffect(()=>{
+    setMounted(true)
+  },[])
+
   if (!mounted) return null;
 
   if (isMaintenance) {
     return (
       <div className="h-screen bg-zinc-900 text-white flex flex-col items-center justify-center text-center px-6">
         <h1 className="text-3xl font-bold mb-2">🚧 Site Under Maintenance</h1>
-        <p className="text-lg mb-4">Will be back by <strong>onday morning</strong>.</p>
+        <p className="text-lg mb-4">Will be back by <strong>monday morning</strong>.</p>
       </div>
     );
   }
