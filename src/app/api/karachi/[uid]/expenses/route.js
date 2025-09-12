93341780838f7a8e33af66239f0d148e6121610c
ubaid1840 <ubaidrehman1840@gmail.com>
2025-09-12 14:09:58 +0500

    office expenses separated for each user

--- src/app/api/karachi/[uid]/expenses/route.js
+++ src/app/api/karachi/[uid]/expenses/route.js
@@ -32,39 +32,46 @@ export async function POST(req) {
 
 
 export async function GET(req) {
+  const searchParams = req.nextUrl.searchParams;
+  const start_date = searchParams.get("start_date");
+  const end_date = searchParams.get("end_date");
+  const { uid } = await params;
 
-  
-    const searchParams = req.nextUrl.searchParams
-    const start_date = searchParams.get('start_date')
-    const end_date = searchParams.get('end_date')
+  const meQuery = await pool.query(`SELECT office FROM users WHERE id = $1`, [
+    uid,
+  ]);
 
-    try {
-        let query = `
+  const office = meQuery.rows[0]?.office || "lahore";
+
+  try {
+    let query = `
     SELECT 
     r.*, 
     u.id AS user_id, 
     u.name AS submitted_by_name
 FROM branchexpenses r
 INNER JOIN users u ON r.submitted_by = u.id
+ WHERE u.office = $1
     `;
 
-        const queryParams = [];
+    const queryParams = [office];
 
-        if (start_date && end_date) {
-            query += ` WHERE r.date BETWEEN $1 AND $2`;
-            queryParams.push(start_date, end_date);
-        }
-        query += ` ORDER BY r.date DESC;`;
-        const result = await pool.query(query, queryParams);
-        return NextResponse.json(result.rows, { status: 200 })
-
-    } catch (error) {
-        console.error('Error fetching data: ', error);
-        return NextResponse.json({ message: error.message || "Something went wrong" }, { status: 500 })
+    if (start_date && end_date) {
+      query += ` AND r.date BETWEEN $1 AND $2`;
+      queryParams.push(start_date, end_date);
     }
-
-
+    query += ` ORDER BY r.date DESC;`;
+    const result = await pool.query(query, queryParams);
+    return NextResponse.json(result.rows, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching data: ", error);
+    return NextResponse.json(
+      { message: error.message || "Something went wrong" },
+      { status: 500 }
+    );
+  }
 }
+
 
 
 export const revalidate = 0
