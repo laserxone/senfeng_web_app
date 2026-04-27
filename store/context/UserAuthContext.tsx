4df4de36ef916f703461966c566a2713982a28ff
ubaid1840 <ubaidrehman1840@gmail.com>
2026-04-27 18:57:33 +0500

    authContext fixed

--- store/context/UserAuthContext.tsx
+++ store/context/UserAuthContext.tsx
@@ -8,7 +8,7 @@ import {
 } from "firebase/auth";
 import { usePathname } from "next/navigation";
 import { useRouter } from 'nextjs-toploader/app';
-import { createContext, useContext, useEffect, useRef, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { toast } from "sonner";
 
 type AuthContextType = {
@@ -17,10 +17,8 @@ type AuthContextType = {
 };
 
 const AuthContext = createContext<AuthContextType>({
-
     loading: true,
     authData: null
-
 });
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
@@ -29,24 +27,16 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     const router = useRouter();
     const pathname = usePathname();
     const [authData, setAuthData] = useState<any | null>(null)
-    const lastUserRef = useRef<string | null>(null);
-    const cacheRef = useRef<Record<string, any>>({});
 
 
     useEffect(() => {
         const unsub = onAuthStateChanged(auth, async (fbUser) => {
             const email = fbUser?.email ?? null;
-            if (lastUserRef.current === email) return;
-            lastUserRef.current = email;
             setLoading(true);
             try {
                 if (email) {
-
-                    if (!cacheRef.current[email]) {
-                        const res = await axios.get(`/userdetail/${email}`);
-                        cacheRef.current[email] = res.data;
-                    }
-                    const userData = cacheRef.current[email];
+                    const res = await axios.get(`/userdetail/${email}`);
+                    const userData = res.data
                     if (userData?.designation) {
                         setAuthData({ ...userData, ...fbUser })
                     } else {
@@ -56,10 +46,12 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
                 } else {
                     setAuthData(null)
                     if (!pathname.includes('login') && !pathname.includes('signup') && !pathname.includes('forgetpassword')) {
+                        setLoading(false)
                         router.replace('/login');
                     }
                 }
             } catch (error) {
+                setLoading(false)
                 setAuthData(null);
                 signOut(auth)
                 router.replace("/login");
