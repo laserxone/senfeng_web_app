4e230a97a049d563d7850ee11d0ccd4a777161dd
ubaid1840 <ubaidrehman1840@gmail.com>
2026-05-19 13:44:10 +0500

    edge panel turned back on

--- app/[city]/layout.tsx
+++ app/[city]/layout.tsx
@@ -24,14 +24,14 @@ export default async function DashboardLayout({ children, params }: {
       <PageContainer>
         {children}
       </PageContainer>
-      {/* <FinePopup /> */}
-      {/* <EdgePanel width={384} className={"pt-4 z-2"}>
+      <FinePopup />
+      <EdgePanel width={384} className={"pt-4 z-2"}>
         <div className="flex gap-2 w-full justify-evenly">
           <FloatingInformation />
           <FloatingTodo />
           <FloatingChat />
         </div>
-      </EdgePanel> */}
+      </EdgePanel>
     </CommonLayout>
   );
 }
