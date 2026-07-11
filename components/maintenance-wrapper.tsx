7f0e0c3138c3deab73f63963c888acda3f885b57
ubaid1840 <ubaidrehman1840@gmail.com>
2026-07-11 12:13:43 +0500

    maintenance turned back on

--- components/maintenance-wrapper.tsx
+++ components/maintenance-wrapper.tsx
@@ -3,7 +3,7 @@ import { ReactNode, useEffect, useState } from 'react';
 
 export default function MaintenanceWrapper({ children }: { children: ReactNode }) {
   const [mounted, setMounted] = useState(false);
-  const [isMaintenance, setIsMaintenance] = useState(false);
+  const [isMaintenance, setIsMaintenance] = useState(true);
 
 
   useEffect(() => {
